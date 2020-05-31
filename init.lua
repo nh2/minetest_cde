35c4f1413b598b209fcf8ad84e8f1b1a7cb6b6f6
Niklas Hambüchen <mail@nh2.me>
2020-05-31 17:06:55 +0200

    Initial commit with Rittersport-Box

---
+++ init.lua
@@ -0,0 +1,11 @@
+minetest.register_node("cde:rittersportbox", {
+  tiles = {
+    "rittersportbox_top.png", -- top
+    "cardboard.png", -- bottom
+    "cardboard.png", -- right
+    "cardboard.png", -- left
+    "cardboard.png", -- back
+    "cardboard.png" -- front
+  },
+  groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2},
+})
